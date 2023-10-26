7130184949604c9c7badd20580bf510331dd3cea
simesaba80 <simesaba1120@gmail.com>
2023-10-26 19:54:41 +0900

    [change]一度リクエスト用に構造体を作成して処理をする形にしました

--- controller/creatuser.go
+++ controller/creatuser.go
@@ -9,11 +9,20 @@ import (
 )
 
 func CreateUser(c echo.Context) error {
+	type Body struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"Password"`
+	}
+	obj := Body{}
 	user := snsdb.User{}
-	if err := c.Bind(&user); err != nil {
+	if err := c.Bind(&obj); err != nil {
 		//return 400
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
+	user.Name = obj.Name
+	user.Email = obj.Email
+	user.Password = obj.Password
 	snsdb.DB.Create(&user)
 	return c.JSON(http.StatusCreated, user)
 }
