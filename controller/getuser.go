d2124616240b75d36a8462b0ee00c9abe5a61d37
simesaba80 <simesaba1120@gmail.com>
2023-10-27 17:50:54 +0900

    [add]ユーザーの単体取得を実装

--- controller/getuser.go
+++ controller/getuser.go
@@ -13,3 +13,18 @@ func GetUsers(c echo.Context) error {
 	snsdb.DB.Find(&users)
 	return c.JSON(http.StatusOK, users)
 }
+
+func GetUser(c echo.Context) error {
+	type Body struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	obj := Body{}
+	user := snsdb.User{}
+	if err := c.Bind(&obj); err != nil {
+		return err
+	}
+	snsdb.DB.Where("Email = ?", obj.Email).Where("Password = ?", obj.Password).First(&user)
+
+	return c.JSON(http.StatusOK, user)
+}
