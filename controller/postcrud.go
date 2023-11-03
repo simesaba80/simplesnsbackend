42f908f627afb4f876fb8b0f7315dc1d4ffce49e
simesaba80 <simesaba1120@gmail.com>
2023-11-03 15:34:40 +0900

    [change]ファイル分割をエンドポイント毎からテーブル毎にしました

--- controller/postcrud.go
+++ controller/postcrud.go
@@ -2,10 +2,30 @@ package controller
 
 import (
 	"net/http"
+
 	snsdb "snsback/db"
 
 	"github.com/labstack/echo/v4"
 )
+
+func Createpost(c echo.Context) error {
+	type Body struct {
+		Content string `json:"content"`
+		UserId  uint   `json:"userid"`
+	}
+	obj := Body{}
+	post := snsdb.Post{}
+	user := snsdb.User{}
+	if err := c.Bind(&obj); err != nil {
+		//return 400
+		return echo.NewHTTPError(http.StatusBadRequest, "BadRequest")
+	}
+	snsdb.DB.Where("ID = ?", obj.UserId).First(&user)
+	post.Content = obj.Content
+	post.UserId = user.ID
+	snsdb.DB.Create(&post)
+	return c.JSON(http.StatusCreated, post)
+}
 
 func GetPosts(c echo.Context) error {
 	//専用の返り値を宣言
