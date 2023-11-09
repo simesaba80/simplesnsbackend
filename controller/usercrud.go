5b3156b36fce40c1052dd065b57d81a7739895bd
simesaba80 <simesaba1120@gmail.com>
2023-11-09 15:06:51 +0900

    [add]ログインできるようにしました

--- controller/usercrud.go
+++ controller/usercrud.go
@@ -6,6 +6,7 @@ import (
 	snsdb "snsback/db"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 
 func CreateUser(c echo.Context) error {
@@ -65,4 +66,34 @@ func UpdateUser(c echo.Context) error {
 
 	snsdb.DB.Model(&user).Updates(snsdb.User{Name: obj.ReName, Email: obj.ReEmail, Password: obj.RePassword})
 	return c.JSON(http.StatusOK, user)
+}
+
+func Login(c echo.Context) error {
+	type Body struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	obj := Body{}
+	if err := c.Bind(&obj); err != nil {
+		// return 400
+		return c.JSON(http.StatusBadRequest, echo.Map{
+			"message": "Json Format Error: " + err.Error(),
+		})
+	}
+	user := snsdb.User{}
+	if err := snsdb.DB.Where("email = ?", obj.Email).First(&user).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			// return 404
+			return c.JSON(http.StatusNotFound, echo.Map{
+				"message": "User Not Found",
+			})
+		} else {
+			// return 500
+			return c.JSON(http.StatusInternalServerError, echo.Map{
+				"message": "Database Error: " + err.Error(),
+			})
+		}
+	}
+	return c.JSON(http.StatusOK, user.Id)
 }
